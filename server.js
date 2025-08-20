12ef5bc6286d45a5c515dd2feee53a6460e3207c
mazzegaDev <mazzega_dev@unoeste.edu.br>
2025-08-19 22:11:01 -0300

    fim

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import usuarioRouter from './routes/usuarioRoute.js';
-const server = express();
 server.use(express.json());//Trasnformar String para JSON
 server.use("/usuario", usuarioRouter);
-
-
+import swaggerUI from 'swagger-ui-express'
+import {createRequire} from 'module'
+const require = createRequire(import.meta.url)
+const outputJson = require("./swaggerOutput.json")
+const server = express();
 
 
 server.listen(5000, function() {
