1c18a951d73dcff0101be1f1c139f3193ae1c1f3
mazzegaDev <mazzega_dev@unoeste.edu.br>
2025-08-19 22:13:03 -0300

    f

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import usuarioRouter from "./routes/usuarioRoute.js";
 server.use(express.json()); //Trasnformar String para JSON
-
+const server = express();
 import swaggerUI from "swagger-ui-express";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const outputJson = require("./swaggerOutput.json");
-const server = express();
+
 server.use("/doc", swaggerUI.serve, swaggerUI.setup(outputJson));
 server.use("/usuario", usuarioRouter);
 
