8aa07c3d41ae923e19e94f2cff74896ee69d684e
MazzegaDev <mazzega_dev@unoeste.edu.br>
2025-08-20 20:35:14 -0300

    fim da primeira aula

--- swagger.js
+++ swagger.js
@@ -7,8 +7,31 @@ const doc = {
     tittle: "API REST - PFS2",
     description: "API REST para a construção do backend na disciplina",
   },
-};
+  components: {
+    schemas: {
+      erro:{
+        msg: "mensagem de erro"
+      },
+      usuario:{
+        nome: "nome do usuario",
+        email: "email do usuario"
+      }
+    }
+  }
+}
 
 const routes = ["./server.js"];
 const outputJson = "./swaggerOutput.json";
 swaggerAutogen({openapi: '3.0.0'})(outputJson, routes, doc)
+.then(async () => {
+  //Quando executa o swagger.js ele documenta nossa API e executa a aplicacao, assim automatizando tudo
+  await import("./server.js")
+})
+
+/*
+    "scripts": 
+    "start": "node swagger.js",
+    essa configuração no package.json faz com oque o npm start execute o swagger e a aplicação
+
+  },
+*/
