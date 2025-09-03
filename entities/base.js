330f4ad7bce501c1f927cbe8bd1d30b5339d2907
mazzegaDev <mazzega_dev@unoeste.edu.br>
2025-09-03 20:37:32 -0300

    anotacção

--- entities/base.js
+++ entities/base.js
@@ -5,6 +5,7 @@ export default class Base{
 
     toJSON(){           
         //getOwnPropertyNames -> pega os atributos da classe que instanciou
+        //Object.getPrototypeOf(this) -> pega o esqueleto da classe que instanciou o metodo
         let props = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
     }
 }
