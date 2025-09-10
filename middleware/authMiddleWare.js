68ae53108100be30db319b80fe417a63c113723c
mazzegaDev <mazzega_dev@unoeste.edu.br>
2025-09-10 20:38:31 -0300

    .

--- middleware/authMiddleWare.js
+++ middleware/authMiddleWare.js
@@ -28,21 +28,23 @@ export default class AuthMiddleware {
       //Verifica se no cabeçalho nosso token existe. Verfica se tem algo no atributo athoriztion.
 
       //Se existe retorna o valor
+      //Aqui estamos pegando o token no cabeçalho da req
       let token = req.headers.authorization.split(" ")[1];
       /*athorization: "Bearer <token> "
       Recorta a string depois do espaço e pega a primeira posição -> split faz um array. */
 
       try {
         //Verify verifica se token e a chave secreta sao validos
+
         //Se deu certo ele decodifica o corpo
-        //Validar as infos do usuario que esta no token
         let payload = jwt.verify(token, secret);
         let usuarioRepository = new UsuarioRepository();
+
         //Valida o usuario pelo id no payload no banco de dados
         let usuario = await usuarioRepository.buscarPorId(payload.id);
-        if (usuario) {
-          if (usuario.ativo) {
-            next();
+        if (usuario) {//se o usuario existe
+          if (usuario.ativo) { // se o usuario esta ativo
+            next();// se ativo, conclui a req
           } else {
             return res.status(401).json({ msg: "Usuario inativo" });
           }
